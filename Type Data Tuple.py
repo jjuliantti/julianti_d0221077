c5639ca7102121245e4b0bf3936b8e98d088b34f
jjuliantti <101525573+jjuliantti@users.noreply.github.com>
2022-03-20 12:47:55 +0800

    Add files via upload

---
+++ Type Data Tuple.py
@@ -0,0 +1,48 @@
+#Type Data Tuple
+#()
+#Intable
+tuple = ("hijau",83,5.97,False)
+
+#perulangan
+for i in (tuple):
+    print (i)
+
+#perulangan while loop
+while i < len(tuple):
+    print (tuple[i])
+    i += 1
+
+#mengupdate nilai/value
+#konversi tuple ke list
+tupl = list(tuple)
+tupl[1] = 27
+print (tupl)
+
+#menghapus nilai/value
+#remove
+#pop
+
+#remove
+tupl.remove("hijau")
+print(tupl)
+
+#pop
+tupl.pop()
+print(tupl)
+
+#menambahkan nilai/value
+#append
+#extend
+#insert
+
+#append
+tupl.append (True)
+print(tupl)
+
+#extend
+tupl.extend (["merah"])
+print(tupl)
+
+#insert
+tupl.insert (1, 3.7)
+print(tupl)
